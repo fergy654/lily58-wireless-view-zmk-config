098665c6262066a1b747497993a7450c8559e8ad
ferggy <pfergie2016@gmail.com>
2026-09-09 00:19:34 -0500

    increase peripheral battery box size

--- config/boards/shields/nice_view/widgets/status.c
+++ config/boards/shields/nice_view/widgets/status.c
@@ -97,7 +97,7 @@ static void draw_peripheral_battery(lv_obj_t *canvas,
 
   snprintf(buf, sizeof(buf), "R:%d%%", state->peripheral_battery);
 
-  lv_canvas_draw_text(canvas, 0, 22, 40, &label_dsc, buf);
+  lv_canvas_draw_text(canvas, 0, 20, 41, &label_dsc, buf);
 
 //   if (state->peripheral_charging) {
 //     lv_draw_img_dsc_t img_dsc;
@@ -132,7 +132,7 @@ static void draw_central_status(lv_obj_t *canvas, const struct status_state *sta
     break;
   }
 
-  lv_canvas_draw_text(canvas, 0, 0, CANVAS_SIZE, &label_dsc, output_text);
+  lv_canvas_draw_text(canvas, 0, 2, CANVAS_SIZE, &label_dsc, output_text);
 }
 
 static void draw_top(lv_obj_t *widget, lv_color_t cbuf[],
